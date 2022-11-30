2a6d5b5885082ee7d8e0135592bfc399b2e8765c
Gerges-Hana <83248914+Gerges-Hana@users.noreply.github.com>
2022-11-30 19:41:49 +0200

    first task for js

---
+++ test.js
@@ -0,0 +1,73 @@
+// #####################  1  #####################3
+// var temperature =parseInt( prompt('enter today’s temperature '));
+
+// // console.log(temperature);
+// let result = (temperature >= 30) ? 'Hot' : 'Cold';
+
+// console.log(`You have ${result} day.`);
+
+
+// ################  2  ##############
+//var user =prompt('enter your name ');
+// 
+// if (isNaN(user)) {
+//     console.log("your user name is a string ")
+
+
+//     var birthYear =prompt('enter your Birth Year ');
+//     if (isFinite(birthYear) && (birthYear < 2010)) {
+//         console.log("your birthYear is a number ")
+//         var age =2022-birthYear;
+//         console.log(age);
+        
+//     } else {
+//         console.log("your birthYear isnot a number ")
+//         for (var i = 0; i < 3; i++) {
+            
+//             var birthYear =prompt('enter your Birth Year ');
+//             if((isFinite(birthYear) && (birthYear < 2010))){
+//                console.log(i)
+//             i=3;
+//             }
+//         }
+
+//     }
+    
+// }
+// else{
+//     console.log("your user name isnot a string ")
+
+// }
+
+// document.getElementById('name').innerHTML=`name : ${user}`;
+// document.getElementById('Birthyear').innerHTML=`Birth year: ${birthYear}`;
+// document.getElementById('Age').innerHTML=`Age : ${age}`;
+
+
+// ############## 3  ###############3
+// var r1,r2;
+// var a =parseInt(prompt('enter a '));
+// var b =parseInt(prompt('enter b '));
+// var c =parseInt(prompt('enter c '));
+
+// // r1 =((-b+(Math.sqrt*b^2-4*a*c))/2*a);مشتغلش معايا الكود دا 
+// r1= -b/(2*a)-(Math.sqrt*b^2-4*a*c);
+// r2= -b/(2*a)+(Math.sqrt*b^2-4*a*c);
+// alert("first number = "+r1)
+// alert("second number = "+r2)
+// //// console.log(r1)
+
+
+// ################ 4  ###########################
+
+// var n=parseInt(prompt('enter first number '));
+// var m=parseInt(prompt('enter second number '));
+// var result = n+m;
+// res2=result%10;
+// console.log(result);
+// console.log(res2);
+
+
+// #############  5  #################### 
+// var d =parseInt(prompt('enter the distance'));
+// alert('the team of distance between car x and y = '+d*2);
